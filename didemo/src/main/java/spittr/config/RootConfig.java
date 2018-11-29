491c14c7e2462db3a088be9271d002d3ac500775
律岩杰 <lvyj001@cmft.com>
2018-11-29 23:49:36 +0800

    chapter10 datesource

--- didemo/src/main/java/spittr/config/RootConfig.java
+++ didemo/src/main/java/spittr/config/RootConfig.java
@@ -1,11 +1,31 @@
 package spittr.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
 @Configuration
 @ComponentScan(basePackages = {"spittr.web"}, excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
 public class RootConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:schema.sql")
+                .addScript("classpath:test-data.sql")
+                .build();
+    }
 }
